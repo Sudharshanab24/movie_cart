72f2504c733cd40fb0c9d75f6d478580001f144c
sudharshana <sudhubalu04@gmail.com>
2024-05-04 11:33:36 +0530

    some changes

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
     server: {
       proxy: {
         '/v1': {
-          target: 'https://api.themoviedb.org/3/discover/movie?api_key=5aaa793034c842de2ed6208b20ad2cb8&with_original_language=ta&sort_by=vote_average.desc&year=2024',
+          target: 'https://api.themoviedb.org/3/discover/movie?api_key=5aaa793034c842de2ed6208b20ad2cb8xxyx&with_original_language=ta&sort_by=vote_average.desc&year=2024',
           changeOrigin: true,
           secure: false,
         },
